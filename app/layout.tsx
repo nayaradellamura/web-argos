aea47a8996249bf56829ee63342973a87f35a10a
nayara.dellamura <nayara.dellamura@varitus.com.br>
2026-04-12 10:09:01 -0300

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,18 +13,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
         url: "/icon.svg",
         type: "image/svg+xml",
       },
     ],
+    shortcut: "/icon.svg",
     apple: "/apple-icon.png",
   },
 };
